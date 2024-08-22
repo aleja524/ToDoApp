fd844fe37f466c27004b715a032ec151077624b9
Alejandra Escobar <aleja.escobar2501@gmail.com>
2024-08-22 11:11:29 -0500

    point 2

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -2,10 +2,12 @@
 
 class Todo:
 
-
     def __init__(self, code_id: int, tittle: str, description: str):
         self.code_id: int = code_id
         self.title: str = tittle
         self.description: str = description
         self.completed: bool = False
         self.tags: list[str] = []
+
+    def mark_completed(self):
+        self.completed = True
